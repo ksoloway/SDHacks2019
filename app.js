061d5f35563ae21665199fac61b7b41c2b9dde7c
ksoloway <ksoloway@ucsd.edu>
2019-10-26 14:02:44 -0700

    Removed code

--- app.js
+++ app.js
@@ -13,23 +13,10 @@ AWS.config.credentials = credentials;
 AWS.config.getCredentials(function(err) {
 	if (err) console.log(err.stack);
 	// credentials not loaded
-	else {
-	  console.log("Access key:", AWS.config.credentials.accessKeyId);
-	  console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
-	}
   });
   AWS.config.update({region: 'us-east-1'});
   var comprehend = new AWS.Comprehend();
-    
-  var params = {
-	Text: 'happy', /* required */
-	LanguageCode: "en" /* required */
-  };
   
-  comprehend.detectSentiment(params, function(err, data) {
-	if (err) console.log(err, err.stack); // an error occurred
-	else     console.log(data);           // successful response
-  });
 const MongodbMemoryServer = require('mongodb-memory-server');
 
 const mongoServer = new MongodbMemoryServer.MongoMemoryServer({
