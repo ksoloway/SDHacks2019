f8ab9e7b9eeeff03d8d1eaa073f791a8e5d9e634
ksoloway <ksoloway@ucsd.edu>
2019-10-27 04:36:57 -0700

    fixed frontpage

--- public/scripts/frontpage.js
+++ public/scripts/frontpage.js
@@ -1,13 +1,37 @@
+
 $(document).ready(function() {
 
-    $("#login").click(function() {
-          location.href = "/loggedin";
-    });
+   
   
     $("#signup").click(function() {
       location.href = "/signup";
   });
 
+  $("#calculate-button").click(function() {
+    var text = $("#analysis-text").val();
+    var language = $("#language-dropdown").val();
+$.ajax({
+  type: "GET",
+  url: "/amzapi/"+text+"/"+language,
+  data: {},
+  success: function(data) {
+    var string = JSON.stringify(data);
+            var objectValue = JSON.parse(string);
+            $("#results-box").html("Result: " + objectValue["Sentiment"]);
+            $("#positive").html("Positive: " + objectValue["SentimentScore"]["Positive"]);
+            $("#negative").html("Negative: " + objectValue["SentimentScore"]["Negative"]);
+            $("#mixed").html("Mixed: " + objectValue["SentimentScore"]["Mixed"]);
+            $("#neutral").html("Neutral: " + objectValue["SentimentScore"]["Neutral"]);
+            }
+});
+});
+$("#clear-button").click(function() {
+  $("#analysis-text").val('');
+
+});
+
     
   });
+  
+  
   
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -15,22 +15,6 @@ window.onclick = function(event) {
   }
 }
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
 $(document).ready(function() {
 
   $.get('/getUser',function(data){
