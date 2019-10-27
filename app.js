399555438635c7629fead195f36b54b2f3067793
Sean Race <srace@ucsd.edu>
2019-10-26 21:46:01 -0700

    changes

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.listen(port, function () {
 
 // Serves the index.html file (our basic frontend)
 app.get('/', function (req, res) {
-	res.sendFile('/views/indextest.html', { root: __dirname });
+	res.sendFile('/views/index.html', { root: __dirname });
 });
 
 app.get('/indextest', function (req, res) {
@@ -92,7 +92,7 @@ function languageCode(language) {
 		return "pt";
 	}
 }
-  
+
 
 app.get('/amzapi/:text/:language', function(req,res,next){
 	textParam = req.params.text;
