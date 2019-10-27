ecae7a8a66fbead64704bf76e6938e349a8a3267
Tianxing Liu <tianxing0220@gmail.com>
2019-10-26 22:00:33 -0700

    signup and login setup

---
+++ models/login.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define a new Schema: specify the types of objects that will be stored in our db
+const LoginSchema = new Schema({
+	username: String,
+	password: String
+	// Friends is a list of IDs (references to other entries)
+});
+
+const Login = mongoose.model("login", LoginSchema);
+
+module.exports = Login; // Export the schema so it can be accessed in App.js
